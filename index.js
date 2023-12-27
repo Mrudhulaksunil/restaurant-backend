3c6509bacd0b28eb05d945ad513c903b2559e792
Mrudhulaksunil <mrudhulaks2001@gmail.com>
2023-12-27 14:03:07 +0530

    Backendfor restaurant app

---
+++ index.js
@@ -0,0 +1,30 @@
+//import json -server
+
+const jsonServer=require('json-server')
+
+//create a server application using json-server
+
+const restServer=jsonServer.create()
+
+//setup path for db.json
+
+const router=jsonServer.router()
+
+//return a middleware used by json-server
+
+const middleware=jsonServer.defaults()
+
+//setup port
+
+const port=3002
+
+//use in server
+
+restServer.use(middleware)
+restServer.use(router)
+
+//to run a port
+
+restServer.listen(port,()=>{
+    console.log('Rest server listening on port'+port);
+})
